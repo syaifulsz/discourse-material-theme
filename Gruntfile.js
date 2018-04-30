bebb534d1f01782158d7fc1afeff4cdbe5501fdc
Syaiful Shah Zinan <i.works@live.com>
2018-04-30 18:11:47 +0800

    Add SASS Linter and update package.json file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
 module.exports = function(grunt) {
-  'use strict'
+    'use strict'
 
-  require('load-grunt-tasks')(grunt)
-  require('time-grunt')(grunt)
+    require('load-grunt-tasks')(grunt)
+    require('time-grunt')(grunt)
 
-  // project configuration
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // project configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
-      options: {
-        banner: '@function brightness($color) {\n' +
+        concat: {
+            options: {
+                banner: '@function brightness($color) {\n' +
                 '  @return ((red($color) * .299) + (green($color) * .587) + (blue($color) * .114));\n' +
                 '}\n' +
                 '\n' +
@@ -30,98 +30,106 @@ module.exports = function(grunt) {
                 '  }\n' +
                 '}\n' +
                 '\n'
-      },
-      common: {
-        dest: 'common/common.scss',
-        src: '<%= sass.common.dest %>'
-      },
-      desktop: {
-        dest: 'desktop/desktop.scss',
-        src: '<%= sass.desktop.dest %>'
-      },
-      mobile: {
-        dest: 'mobile/mobile.scss',
-        src: '<%= sass.mobile.dest %>'
-      }
-    },
+            },
+            common: {
+                dest: 'common/common.scss',
+                src: '<%= sass.common.dest %>'
+            },
+            desktop: {
+                dest: 'desktop/desktop.scss',
+                src: '<%= sass.desktop.dest %>'
+            },
+            mobile: {
+                dest: 'mobile/mobile.scss',
+                src: '<%= sass.mobile.dest %>'
+            }
+        },
 
-    postcss: {
-      options: {
-        map: false,
-        processors: [
-          require('autoprefixer')({
-            browsers: [
-              'last 1 major version',
-              '>= 1%',
-              'Android >= 4.4',
-              'Chrome >= 45',
-              'Edge >= 12',
-              'Explorer >= 10',
-              'Firefox >= 38',
-              'iOS >= 9',
-              'Opera >= 30',
-              'Safari >= 9'
-            ]
-          })
-        ]
-      },
-      common: {
-        src: 'common/common.scss'
-      },
-      desktop: {
-        src: 'desktop/desktop.scss'
-      },
-      mobile: {
-        src: 'mobile/mobile.scss'
-      }
-    },
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('autoprefixer')({
+                        browsers: [
+                            'last 1 major version',
+                            '>= 1%',
+                            'Android >= 4.4',
+                            'Chrome >= 45',
+                            'Edge >= 12',
+                            'Explorer >= 10',
+                            'Firefox >= 38',
+                            'iOS >= 9',
+                            'Opera >= 30',
+                            'Safari >= 9'
+                        ]
+                    })
+                ]
+            },
+            common: {
+                src: 'common/common.scss'
+            },
+            desktop: {
+                src: 'desktop/desktop.scss'
+            },
+            mobile: {
+                src: 'mobile/mobile.scss'
+            }
+        },
 
-    sass: {
-      options: {
-        precision: 6,
-        // sourcemap: 'none',
-        style: 'expanded'
-      },
-      common: {
-        dest: 'common/common.scss',
-        src: 'scss/common.scss'
-      },
-      desktop: {
-        dest: 'desktop/desktop.scss',
-        src: 'scss/desktop.scss'
-      },
-      mobile: {
-        dest: 'mobile/mobile.scss',
-        src: 'scss/mobile.scss'
-      }
-    },
+        sass: {
+            options: {
+                precision: 6,
+                // sourcemap: 'none',
+                style: 'expanded'
+            },
+            common: {
+                dest: 'common/common.scss',
+                src: 'scss/common.scss'
+            },
+            desktop: {
+                dest: 'desktop/desktop.scss',
+                src: 'scss/desktop.scss'
+            },
+            mobile: {
+                dest: 'mobile/mobile.scss',
+                src: 'scss/mobile.scss'
+            }
+        },
 
-    scsslint: {
-      options: {
-        config: 'scss/.scss-lint.yml'
-      },
-      project: ['scss/**/*.scss']
-    },
+        // scsslint: {
+        //     options: {
+        //         config: 'scss/.scss-lint.yml'
+        //     },
+        //     project: ['scss/**/*.scss']
+        // },
 
-    // update package.json packages
-    devUpdate: {
-      default: {
-        options: {
-          semver: false,
-          updateType: 'prompt'
+        sasslint: {
+            // options: {
+            //     configFile: 'scss/.sass-lint.yml',
+            // },
+            target: ['scss/webboard/**/*.scss']
+        },
+
+        // update package.json packages
+        devUpdate: {
+            default: {
+                options: {
+                    semver: false,
+                    updateType: 'prompt'
+                }
+            }
         }
-      }
-    }
-  })
+    })
 
-  // task registration
-  grunt.registerTask(
-    'default',
-    [
-      // 'scsslint',
-      'sass',
-      'concat',
-      'postcss'
-    ]
-  )
+    // task registration
+    grunt.registerTask(
+        'default',
+        [
+            'sasslint',
+            // 'scsslint',
+            'sass',
+            'concat',
+            'postcss'
+        ]
+    )
 }
