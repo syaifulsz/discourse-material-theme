3da979112931e69fa09d2a71935dd8f62a9fb119
Syaiful Shah Zinan <i.works@live.com>
2018-05-02 11:50:05 +0800

    Update CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
         concat: {
             options: {
                 banner: '$webboard__category_newcar: \'\' !default;\n' +
+                '$webboard__category_motor: \'\' !default;\n' +
                 '$webboard__icon_sprite: \'\' !default;\n' +
                 '@function brightness($color) {\n' +
                 '  @return ((red($color) * .299) + (green($color) * .587) + (blue($color) * .114));\n' +
